817bd8e4a3279f355e9001abb9ff9ecacd0bde3c
biluo <ningbiluoer@126.com>
2026-06-22 17:50:30 +0800

    初始化菜单

--- player-ui/src/App.vue
+++ player-ui/src/App.vue
@@ -1,5 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('0')
+function handleSelect(key, keyPath) {
+  console.log(key, keyPath)
+}
+</script>
 <template>
   <el-config-provider locale="zh-cn">
-    <router-view />
+    <el-header>
+      <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          :ellipsis="false"
+          @select="handleSelect"
+          router
+      >
+        <el-menu-item index="0">🎵 ABC谱播放器</el-menu-item>
+        <el-sub-menu index="1">
+          <template #title>简谱转ABC</template>
+          <el-menu-item index="1-1">🍅番茄简谱</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="2">
+          <template #title>其他</template>
+          <el-menu-item index="2-1">🕳️洞洞谱</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <router-view />
+    </el-main>
+    <el-footer>foot</el-footer>
   </el-config-provider>
 </template>
+
+<style lang="less" scoped>
+.el-menu--horizontal > .el-menu-item:nth-child(1) {
+  margin-right: auto;
+}
+</style>
--- player-ui/src/views/Home.vue
+++ player-ui/src/views/Home.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { ref } from 'vue'
+
+const abcText = ref(`X:1
+T:小星星
+M:4/4
+L:1/4
+K:C
+C C G G | A A G2 | F F E E | D D C2 |]
+`)
 </script>
 
 <template>
   <div class="home-container">
-    home
+    <el-input
+        v-model="abcText"
+        style="width: 45%"
+        :rows="20"
+        type="textarea"
+        placeholder="在这里输入ABC格式的乐谱..."
+    />
   </div>
 </template>
+
+<style lang="less" scoped>
+
+</style>
